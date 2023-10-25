01b270dfe92e04a7b6aab83640ce2e4324b74986
Kshivashankar12 <kshivashankargowda1999@gmail.com>
2023-10-25 18:35:30 +0530

    Add project files.

---
+++ Phase1Section5.12 Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Phase1Section5._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+
+            Teacher teacher = new Teacher();
+            teacher.Name = "Mr.ramesh";
+            teacher.Address = "chennai";
+            teacher.DateOfJoining = DateTime.Today;
+
+            Student student = new Student();
+            student.Name = "Shiva";
+            student.Address = "Bangalore";
+            student.DateOfJoining = DateTime.Today;
+
+            OfficeStaff staff = new OfficeStaff();
+            staff.Name = "Mr.Shankar";
+            staff.Address = "Hyderbad";
+            staff.DateOfJoining = DateTime.Today;
+
+
+            Console.WriteLine(teacher.getInfo() + "\n" + teacher.getTypeOfPerson());
+            Console.WriteLine(student.getInfo() + "\n" + student.getTypeOfPerson());
+            Console.WriteLine(staff.getInfo() + "\n" + staff.getTypeOfPerson());
+        }
+    }
+}
